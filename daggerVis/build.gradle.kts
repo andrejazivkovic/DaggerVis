e895049d70ee6fce217db1538124f32f5b2c0c41
andrejazhivkovikj <andreja.zhivkovikj@wsa.com>
2024-09-26 20:09:20 +0200

    - gitignore + gradle maven publishing

--- daggerVis/build.gradle.kts
+++ daggerVis/build.gradle.kts
@@ -1,54 +1,36 @@
 plugins {
-    alias(libs.plugins.androidLibrary)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    id("java-library")
+    alias(libs.plugins.org.jetbrains.kotlin.jvm)
     alias(libs.plugins.ksp)
-    alias(libs.plugins.power.assert)
-    alias(libs.plugins.kotlinBinary)
-    id("maven-publish")
+    `maven-publish`
 }
-
-android {
-    namespace = "com.andrejaziv.daggergraphvis"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-}
-
-dependencies{
+dependencies {
     implementation(libs.dagger)
     ksp(libs.dagger.compiler)
     implementation(libs.ksp.symbol.processing.api)
     implementation(libs.graphviz.kotlin)
 }
 
+buildTypes {
+    release {
+        isMinifyEnabled = false
+        proguardFiles(
+            getDefaultProguardFile("proguard-android-optimize.txt"),
+            "proguard-rules.pro"
+        )
+    }
+}
 publishing {
-    publications {
-        create<MavenPublication>("release") {
+    publications{
+        register<MavenPublication>("release"){
             afterEvaluate{
                 from(components["release"])
             }
         }
     }
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
