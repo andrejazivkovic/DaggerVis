e3489160b48d97e52d49baa8bf3cdae368e6ba9b
andrejazhivkovikj <andreja.zhivkovikj@wsa.com>
2024-09-26 18:04:16 +0200

    - maven jitpack config test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation(libs.ui.graphics)
     implementation(libs.ui.tooling.preview)
     implementation(libs.material3)
+    implementation(libs.daggervis)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
