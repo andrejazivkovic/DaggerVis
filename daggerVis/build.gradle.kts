c1e3fd5bc20867045f8a17f207e7ae1cfee43fc0
andrejazhivkovikj <andreja.zhivkovikj@wsa.com>
2024-09-26 20:20:04 +0200

    - removed gradle maven publishing + removed jitpack.yml

--- daggerVis/build.gradle.kts
+++ daggerVis/build.gradle.kts
@@ -2,32 +2,12 @@ plugins {
     id("java-library")
     alias(libs.plugins.org.jetbrains.kotlin.jvm)
     alias(libs.plugins.ksp)
-    `maven-publish`
 }
 dependencies {
     implementation(libs.dagger)
     ksp(libs.dagger.compiler)
     implementation(libs.ksp.symbol.processing.api)
     implementation(libs.graphviz.kotlin)
-}
-
-buildTypes {
-    release {
-        isMinifyEnabled = false
-        proguardFiles(
-            getDefaultProguardFile("proguard-android-optimize.txt"),
-            "proguard-rules.pro"
-        )
-    }
-}
-publishing {
-    publications{
-        register<MavenPublication>("release"){
-            afterEvaluate{
-                from(components["release"])
-            }
-        }
-    }
 }
 
 java {
