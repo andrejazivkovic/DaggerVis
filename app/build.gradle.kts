3c6ea39dfc31eac858ab073fc8d7ecbb5c4fd8ba
andrejazhivkovikj <andreja.zhivkovikj@wsa.com>
2024-09-28 02:41:02 +0200

    - Removed unneeded plugin in app - Dump java version to 21 in app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     alias(libs.plugins.org.jetbrains.kotlin.android)
     alias(libs.plugins.composeCompiler)
     alias(libs.plugins.ksp)
-    `maven-publish`
 }
 
 android {
@@ -33,11 +32,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "21"
     }
     buildFeatures {
         compose = true
